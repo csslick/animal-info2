82ae234f549a2236800f37e2591210d665fb28f2
04-Main <digitone@naver.com>
2021-07-05 16:02:59 +0900

    동물 type별 조회

--- main.js
+++ main.js
@@ -30,13 +30,74 @@ const petData = [
     name: "이쁜이",
     type: "dog"
   },
+  { 
+    imgUrl: 'images/cat01.jpg',
+    name: "야옹이1",
+    type: "cat"
+  },
+  { 
+    imgUrl: 'images/cat02.jpg',
+    name: "야옹이2",
+    type: "cat"
+  },
+  { 
+    imgUrl: 'images/cat03.jpg',
+    name: "야옹이3",
+    type: "cat"
+  },
+  { 
+    imgUrl: 'images/cat04.jpg',
+    name: "야옹이4",
+    type: "cat"
+  },
+  { 
+    imgUrl: 'images/cat05.jpg',
+    name: "야옹이5",
+    type: "cat"
+  },
+  { 
+    imgUrl: 'images/cat06.jpg',
+    name: "야옹이6",
+    type: "cat"
+  },
+  { 
+    imgUrl: 'images/bird01.jpg',
+    name: "새1",
+    type: "bird"
+  },
+  { 
+    imgUrl: 'images/bird02.jpg',
+    name: "새2",
+    type: "bird"
+  },
+  { 
+    imgUrl: 'images/bird03.jpg',
+    name: "새3",
+    type: "bird"
+  },
+  { 
+    imgUrl: 'images/bird04.jpg',
+    name: "새4",
+    type: "bird"
+  },
+  { 
+    imgUrl: 'images/bird05.jpg',
+    name: "새5",
+    type: "bird"
+  },
+  { 
+    imgUrl: 'images/bird06.jpg',
+    name: "새6",
+    type: "bird"
+  },
 ];
 
 $(function(){
     var html = ''; // html 변수
     // 동물 데이터 불러오기
     for(var i = 0; i < petData.length; i++) {
-      html += `
+      if(petData[i].type == 'dog'){
+        html += `
         <li>
           <a href="#">
             <img 
@@ -47,7 +108,8 @@ $(function(){
               <h4>${petData[i].name}</h4>
             </div>
           </a>
-        </li>`
+        </li>` 
+      }
     }
 
     // html 목록 출력
